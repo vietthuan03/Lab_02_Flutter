bfdde353e22bb14a5cdf62585d280a81adf13c66
vietthuan03 <105033258+vietthuan03@users.noreply.github.com>
2024-08-24 21:39:27 +0700

    mi_card

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            children: <Widget>[
+              CircleAvatar(
+                radius: ,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
